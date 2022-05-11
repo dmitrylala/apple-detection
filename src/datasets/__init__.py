0da48325bb1a39daa76d0b450345bce1d55fedbc
dmitrylala <dim.nesterov2015@gmail.com>
2022-05-11 05:23:20 +0300

    update

--- src/datasets/__init__.py
+++
@@ -1,2 +0,0 @@
-from .fuji import FujiApples, FUJI_IMSIZE
-from .stavsad import StavsadApples, STAVSAD_MODES, STAVSAD_CUT
--- src/utils/__init__.py
+++
@@ -1,1 +0,0 @@
-from .utils import get_train_config, collate_fn, base64_2_mask, mask_2_base64
