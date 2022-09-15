1bfece2c6303d8e7b94b5a555e1121e70d4e0a4c
dmitrylala <dim.nesterov2015@gmail.com>
2022-09-15 18:05:08 +0300

    patches generation

--- src/generate/stavsad_patches.py
+++ src/generate/stavsad_patches.py
@@ -1,12 +1,10 @@
 import argparse
-import asyncio
 import json
 import os
-from aiomultiprocess import Pool
 
 import torch
 from torchvision.transforms.functional import to_pil_image
-from alive_progress import alive_bar, alive_it
+from alive_progress import alive_bar
 
 from src.datasets import StavsadApples, FUJI_IMSIZE, STAVSAD_MODES, STAVSAD_CUT
 from src.utils.generate import get_masks_info, get_n_patches, get_strides
@@ -21,7 +19,7 @@ IMG_EXT = '.jpg'
 MASK_EXT = '.json'
 
 
-async def process(image_name, image, masks, image_folder, masks_folder, strides, n_patches):
+def process(image_name, image, masks, image_folder, masks_folder, strides, n_patches):
     # convert image and masks to batch with size 1
     image_b = image.unsqueeze(0)
     masks_b = masks.unsqueeze(0)
@@ -31,31 +29,33 @@ async def process(image_name, image, masks, image_folder, masks_folder, strides,
     masks_patches = patchify(masks_b, KERNEL_SIZE, strides)
 
     data_info_part = {}
-    for j, (img_patch, masks_patch) in enumerate(zip(image_patches, masks_patches)):
-        patch_name = image_name + f'_{j}'
-        img_name = patch_name + IMG_EXT
-        mask_json_name = patch_name + MASK_EXT
+    with alive_bar(len(image_patches), title=f"Image {image_name} patches") as bar:
+        for j, (img_patch, masks_patch) in enumerate(zip(image_patches, masks_patches)):
+            patch_name = image_name + f'_{j}'
+            img_name = patch_name + IMG_EXT
+            mask_json_name = patch_name + MASK_EXT
 
-        # save image patch
-        patch_path = os.path.join(image_folder, img_name)
-        to_pil_image(img_patch).save(patch_path)
+            # save image patch
+            patch_path = os.path.join(image_folder, img_name)
+            to_pil_image(img_patch).save(patch_path)
 
-        # get and save nonzero masks patches
-        nonzero_masks = masks_patch[torch.sum(masks_patch, dim=(1, 2)) != 0, :, :].numpy()
+            # get and save nonzero masks patches
+            nonzero_masks = masks_patch[torch.sum(masks_patch, dim=(1, 2)) != 0, :, :].numpy()
 
-        patch_info = get_masks_info(nonzero_masks)
-        patch_info['global_offset'] = [(j % n_patches[1]) * strides[1], (j // n_patches[1]) * strides[0]]
-        patch_json_path = os.path.join(masks_folder, mask_json_name)
+            patch_info = get_masks_info(nonzero_masks)
+            patch_info['global_offset'] = [(j % n_patches[1]) * strides[1], (j // n_patches[1]) * strides[0]]
+            patch_json_path = os.path.join(masks_folder, mask_json_name)
 
-        with open(patch_json_path, 'w') as f:
-            json.dump(patch_info, f)
+            with open(patch_json_path, 'w') as f:
+                json.dump(patch_info, f)
 
-        data_info_part[img_name] = mask_json_name 
+            data_info_part[img_name] = mask_json_name 
+            bar()
 
     return data_info_part
 
 
-async def main(n_jobs: int, root_ds: str, src_mode: str):
+def main(root_ds: str, src_mode: str):
     if src_mode not in STAVSAD_MODES + STAVSAD_CUT:
         raise ValueError(f"Bad mode: {src_mode}")
 
@@ -78,15 +78,13 @@ async def main(n_jobs: int, root_ds: str, src_mode: str):
     data_info = {}
     json_path = os.path.join(root, mode + '.json')
 
-    inputs = (
+    args = (
         (ds.get_img_name(i), image, target['masks'], image_folder, masks_folder, strides, n_patches)
         for i, (image, target) in enumerate(ds)
     )
-    with alive_bar(len(ds), title="Images processed") as bar:
-        async with Pool(processes=n_jobs) as pool:
-            async for data_info_part in pool.starmap(process, inputs):
-                data_info.update(data_info_part)
-                bar()
+    for inputs in args:
+       data_info_part = process(*inputs)
+       data_info.update(data_info_part) 
 
     with open(json_path, 'w') as f:
         json.dump(data_info, f)
@@ -94,8 +92,7 @@ async def main(n_jobs: int, root_ds: str, src_mode: str):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Get patches from stavsad dataset")
-    parser.add_argument('-n_jobs', type=int, help="Number of processes")
     parser.add_argument('root_ds', type=str, help="Path to 'stavsad' directory")
     parser.add_argument('-m', dest='src_mode', type=str, help="'train', 'train_cut', 'val', 'val_cut', 'all' or 'all_cut'")
     args = parser.parse_args()
-    asyncio.run(main(**vars(args)))
+    main(**vars(args))
