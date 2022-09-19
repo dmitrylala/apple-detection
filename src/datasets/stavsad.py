7ca5380f5b31b50963654d0c2c83b1ab83db4b5d
dmitrylala <dim.nesterov2015@gmail.com>
2022-09-19 15:49:49 +0300

    added loading with nn predicts

--- src/datasets/stavsad.py
+++ src/datasets/stavsad.py
@@ -10,13 +10,13 @@ from torchvision.transforms.functional import pil_to_tensor, convert_image_dtype
 
 from src.utils import base64_2_mask
 
-
 STAVSAD_MODES = ('train', 'val', 'all')
 STAVSAD_CUT = ('train_cut', 'val_cut', 'all_cut')
 STAVSAD_PATCHES = (
     'train_patches', 'train_cut_patches',
     'val_patches', 'val_cut_patches',
-    'all_patches', 'all_cut_patches'
+    'all_patches', 'all_cut_patches',
+    'all_patches_pred'
 )
 STAVSAD_ALL = STAVSAD_MODES + STAVSAD_CUT + STAVSAD_PATCHES
 
@@ -27,22 +27,22 @@ class StavsadApples(Dataset):
     """
     Dataset with stavsad apples
     """
-    def __init__(self, root: str, mode: str, transforms=None, thres_apples=NUM_APPLES_THRES):
+    def __init__(
+        self, root: str, mode: str, transforms=None, thres_apples=NUM_APPLES_THRES,
+        masks_dir: str = "masks"    
+    ):
         """
         Constructor of stavsad apples dataset
         :param root:
         :param mode:
         :param transforms:
         """
-        if mode not in STAVSAD_ALL:
-            raise ValueError(f"Bad mode: {mode}")
-
         if mode not in os.listdir(root):
             raise ValueError(f"No folder for such mode: {mode}")
 
         self.root = os.path.join(root, mode)
         self.image_folder = os.path.join(self.root, "images")
-        self.masks_folder = os.path.join(self.root, "masks")
+        self.masks_folder = os.path.join(self.root, masks_dir)
 
         json_path = os.path.join(self.root, mode + '.json')
         with open(json_path) as f:
@@ -95,7 +95,10 @@ class StavsadApples(Dataset):
         masks = torch.as_tensor(masks, dtype=torch.uint8)
 
         image_id = torch.tensor([idx])
-        area = (bboxes[:, 3] - bboxes[:, 1]) * (bboxes[:, 2] - bboxes[:, 0])
+        if bboxes.sum() == 0:
+            area = 0
+        else:
+            area = (bboxes[:, 3] - bboxes[:, 1]) * (bboxes[:, 2] - bboxes[:, 0])
         iscrowd = torch.zeros((len(bboxes),), dtype=torch.int64)
 
         target = {
